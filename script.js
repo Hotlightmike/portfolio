d3c252169b16f49ff1840f22dc448c4e87826f5f
Hotlightmike <hotlightmike@gmail.com>
2026-07-14 17:08:35 +0200

    Обновил портфолио

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Мой первый JavaScript!");
+document.querySelector("#about").addEventListener("click", function () {
+    document.querySelector("#text").textContent =
+        "Меня зовут Mykhailo. Я изучаю веб-разработку.";
+});
+
+document.querySelector("#projects").addEventListener("click", function () {
+    document.querySelector("#projects-section").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.querySelector("#contact").addEventListener("click", function () {
+    document.querySelector("#text").textContent =
+        "Email: hotlightmike@gmail.com";
+});
